8ee9a16de67979a8a9e1294038a6795eab553cab
Leon Stanić <108356050+LeonStanic@users.noreply.github.com>
2022-12-10 12:55:28 +0100

    second commit
    Add: counter, click button

--- counter.js
+++ counter.js
@@ -1,13 +1,15 @@
 let repo = 0
 let clickerValue = 1
 
+const balance = document.getElementById("balance")
 const Clicker = document.getElementById("clicker")
+balance.innerText = "Balance: " + repo;
 Clicker.addEventListener("click", repoadd )
 
 function repoadd(){
     repo+=clickerValue
     console.log(repo)
+    balance.innerText = "Balance: " + repo;
 }
 
-const balance = document.getElementById("balance")
-balance.innerText = "Balance: " + repo;
+
