70228b11969bcb85fdee7598c04f56920d395c79
Leon Stanić <108356050+LeonStanic@users.noreply.github.com>
2022-12-11 02:47:10 +0100

    done

--- counter.js
+++ counter.js
@@ -1,23 +1,35 @@
-let repo=0,perSecond=0,storage=100,cjevovod=1, cjevovodCounter=0, cjevovodCijena= 50,nepreradjena=0, preradjena=0, kvaliteta=10, pumpa=1, pumpaCijena=100, spremnici=1, spremniciCijena=10
+let kolona=1, kolonaCijena=75, repo=0,perSecond=0,storage=100,cjevovod=1, cjevovodCijena= 50,nepreradjena=0, preradjena=0, kvaliteta=10, pumpa=1, pumpaCijena=100, spremnici=1, spremniciCijena=10
 
 const balance = document.getElementById("balance")
 const Clicker = document.getElementById("clicker")
 const Novci = document.getElementById("novci")
 const Spremnik = document.getElementById("upgrade1")
-const Pumpa = document.getElementById("upgrade2")
-const Cjevovod = document.getElementById("upgrade3")
+var SpremnikOpis = document.querySelector("#upgrade1desc")
+const Pumpa = document.querySelector("#upgrade2")
+const Cjevovod = document.querySelector("#upgrade3")
+const Kolona = document.querySelector("#upgrade4")
 const Save = document.querySelector(".refinery")
 const Load = document.querySelector(".stanic")
+const vid = document.querySelector("#background-video")
 var username = localStorage.getItem("usernamee")
+var PumpaOpis = document.querySelector("#upgrade2desc")
+var CjevovodOpis = document.querySelector("#upgrade3desc")
+var KolonaOpis = document.querySelector("#upgrade4desc")
+Load.innerHTML = username
+console.log(Load.innerHTML)
 
-console.log(username)
+
+SpremnikOpis.innerHTML = "Povećava maksimalnu količinu nafte (" + spremnici + ")"
+PumpaOpis.innerHTML = "Povećava količinu nafte dobivene po kliku (" + pumpa + ")"
+CjevovodOpis.innerHTML = "Povećava količinu nafte prerađenu svakih 5 sekundi (" + cjevovod + ")"
+KolonaOpis.innerHTML = "Povećava vrijednost nafte (" + kolona + ")"
+
 
 Load.addEventListener('click', ()=>{
     repo = parseInt(localStorage.getItem("repo"))
     perSecond = parseInt(localStorage.getItem("perSecond" ))
     storage = parseInt(localStorage.getItem("storage"))
     cjevovod = parseInt(localStorage.getItem("cjevovod"))
-    cjevovodCounter = parseInt(localStorage.getItem("cjevovodCounter"))
     cjevovodCijena = parseInt(localStorage.getItem("cjevovodCijena"))
     nepreradjena = parseInt(localStorage.getItem("nepreradjena"))
     preradjena = parseInt(localStorage.getItem("preradjena"))
@@ -26,33 +38,40 @@ Load.addEventListener('click', ()=>{
     pumpaCijena = parseInt(localStorage.getItem("pumpaCijena"))
     spremnici = parseInt(localStorage.getItem("spremnici"))
     spremniciCijena = parseInt(localStorage.getItem("spremniciCijena"))
-    username = toString(localStorage.getItem("usernamee"))
+    username = localStorage.getItem("usernamee")
     console.log("Loaded")
     console.log(username)
-    console.log(toString(localStorage.getItem("usernamee")))
 })
 Spremnik.addEventListener('click', ()=>{
     if(repo>=spremniciCijena){
         spremnici++
         storage=100*spremnici
         spremniciCijena*=2
+        console.log(spremnici)
     }
+    SpremnikOpis.innerHTML = "Povećava maksimalnu količinu nafte (" + spremnici + ")"
 })
 Pumpa.addEventListener('click', ()=>{
     if(repo>=pumpaCijena){
         pumpa++
         pumpaCijena*=2
     }
+    PumpaOpis.innerHTML = "Povećava količinu nafte dobivene po kliku (" + pumpa + ")"
+    console.log(pumpa)
 })
 Cjevovod.addEventListener('click', ()=>{
-    if(repo>=0 && cjevovodCounter<4){
-        cjevovod+=2;
+    if(repo>=cjevovodCijena){
+        cjevovod++;
         cjevovodCijena*=2;
-        cjevovodCounter++
     }
-    if(repo>=0 && cjevovodCounter>=4){
-        console.log("ne valja")
+    CjevovodOpis.innerHTML = "Povećava količinu nafte prerađenu svakih 5 sekundi (" + cjevovod + ")"
+})
+Kolona.addEventListener('click', ()=>{
+    if(repo>=kolonaCijena){
+        kolona++;
+        kolonaCijena*=2;
     }
+    KolonaOpis.innerHTML = "Povećava vrijednost nafte (" + kolona + ")"
 })
 
 
@@ -86,12 +105,14 @@ function preradjenoupare(){
     if(nepreradjena-cjevovod>=0){
     preradjena=cjevovod
     nepreradjena-=cjevovod
+    kvaliteta*=kolona
     repo+=preradjena*kvaliteta
     preradjena=0
     }
     if(nepreradjena<cjevovod && nepreradjena>0){
         preradjena=nepreradjena
         nepreradjena=0
+        kvaliteta*=kolona
         repo+=preradjena*kvaliteta
         preradjena=0
     }
@@ -101,7 +122,6 @@ Save.addEventListener('click', ()=>{
     localStorage.setItem("perSecond", perSecond)
     localStorage.setItem("storage", storage)
     localStorage.setItem("cjevovod", cjevovod)
-    localStorage.setItem("cjevovodCounter", cjevovodCounter)
     localStorage.setItem("cjevovodCijena", cjevovodCijena)
     localStorage.setItem("nepreradjena", nepreradjena)
     localStorage.setItem("preradjena", preradjena)
@@ -110,22 +130,29 @@ Save.addEventListener('click', ()=>{
     localStorage.setItem("pumpaCijena", pumpaCijena)
     localStorage.setItem("spremnici", spremnici)
     localStorage.setItem("spremniciCijena", spremniciCijena)
-    localStorage.setItem("usernamee", toString(username))
+    localStorage.setItem("usernamee", username)
     console.log("Saved")
+    console.log(username)
 })
 
-const usernameModal = document.querySelector('.username-modal');
+const usernameModal = document.getElementById('neznamvise');
 const overlay = document.querySelector('.overlay');
 const nastavibutton = document.getElementById("nastavibutton")
 overlay.classList.toggle("none")
-if(Load.innerHTML="Username"){
+if(!username){
+    overlay.classList.toggle("none")
+console.log(Load.innerHTML)
 window.addEventListener('DOMContentLoaded',()=>{
      usernameModal.classList.toggle("active");
      overlay.classList.toggle("active")
+    vid.play()
 })
-}
+
 nastavibutton.addEventListener('click',()=>{
     usernameModal.classList.toggle("active");
     overlay.classList.toggle("active");
+    overlay.classList.toggle("none")
     Load.innerHTML = document.getElementById("myusername").value
+    username = document.getElementById("myusername").value
 })
+}
